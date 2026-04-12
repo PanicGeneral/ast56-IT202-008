8bb7beddfdc0cdf3e5b782a84862ee664321ad76
PanicGeneral <taylorashton54@gmail.com>
2026-04-12 17:42:24 -0400

    added helper functions and navigation template

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -39,9 +39,9 @@ if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
         echo "Email must not be empty<br>";
         $hasError = true;
     }
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email address<br>";
+    $email = sanitize_email($email);
+    if (!is_valid_email($email)) {
+        echo "Invalid email address";
         $hasError = true;
     }
 
